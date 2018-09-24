5d23cb05f669f0a5ca5f792a2142925f8c4f8436
Dmitry Bakulin <dmitrybakulin@MacBook-Pro-Dmitry.local>
2018-09-24 17:47:07 +0300

    logging ViewController AppDelegate

---
+++ Chat/LoggingLifeCycle.swift
@@ -0,0 +1,13 @@
+//
+//  LoggingLifeCycle.swift
+//  Chat
+//
+//  Created by Dmitry Bakulin on 24.09.2018.
+//  Copyright © 2018 Dmitry Bakulin. All rights reserved.
+//
+
+import Foundation
+
+class LoggingLifeCycle {
+    
+}
--- Chat/ViewController.swift
+++ Chat/ViewController.swift
@@ -9,17 +9,51 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    func logMethod(methodName: String = #function) {
+        print("ViewController method: <\(methodName)>")
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        logMethod()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+        logMethod()
         // Dispose of any resources that can be recreated.
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        logMethod()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        logMethod()
+    }
 
+    override func viewWillLayoutSubviews() {
+        logMethod()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        logMethod()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        logMethod()
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        logMethod()
+    }
+    
+    
 }
 
