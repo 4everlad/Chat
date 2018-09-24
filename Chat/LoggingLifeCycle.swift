636dd1554498f1af285776b84a49966b87df6c15
Dmitry Bakulin <dmitrybakulin@MacBook-Pro-Dmitry.local>
2018-09-24 21:39:55 +0300

    LoggingLifeCycle + AppIcon

--- Chat/LoggingLifeCycle.swift
+++ Chat/LoggingLifeCycle.swift
@@ -6,8 +6,56 @@
 //  Copyright © 2018 Dmitry Bakulin. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
 class LoggingLifeCycle {
+    
+    static var previousState =  UIApplication.shared.applicationState
+    static var currentState =  UIApplication.shared.applicationState
+    
+    static var isLogging = true
+    
+    static func logStateAndMethod(methodName: String = #function){
+        if isLogging {
+            var prevState: String
+            var curState: String
+            
+            currentState =  UIApplication.shared.applicationState
+            
+            switch previousState {
+            case .active:
+                prevState = "active"
+            case .inactive:
+                prevState = "inactive"
+            case .background:
+                prevState = "background"
+                
+            }
+            
+            switch currentState {
+            case .active:
+                curState = "active"
+            case .inactive:
+                curState = "inactive"
+            case .background:
+                curState = "background"
+                
+            }
+            
+            print("Application moved from <\(prevState)> to <\(curState)>: <\(methodName)>")
+            
+            previousState = currentState
+        } else {
+            print("Logging is off")
+        }
+    }
+    
+    static func logMethod(methodName: String = #function) {
+        if isLogging {
+            print("ViewController method: <\(methodName)>")
+        } else {
+            print("Logging is off")
+        }
+    }
     
 }
