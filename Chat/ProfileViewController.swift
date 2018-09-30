3c5317e439d5bc82d6050ccc6a655aed9020c4aa
Dmitry Bakulin <d.s.bakulin@gmail.com>
2018-09-30 23:23:05 +0300

    actionSheet

--- Chat/ProfileViewController.swift
+++ Chat/ProfileViewController.swift
@@ -8,14 +8,78 @@
 
 import UIKit
 
-class ProfileViewController: UIViewController {
+class ProfileViewController: UIViewController, UIImagePickerControllerDelegate,
+UINavigationControllerDelegate {
     
     
     @IBOutlet weak var editButton: UIButton!
-    
 
+    @IBOutlet weak var imagePicked: UIImageView!
+    
     @IBAction func chooseImageProfile(_ sender: UIButton) {
-        print("Выбери изображение профиля")
+        print("Choose Image Profile")
+        
+        let alertController = UIAlertController(title: "Выберите действие", message: "", preferredStyle: .actionSheet)
+        
+        let setFromGallery = UIAlertAction(title: "Установить из галлереи", style: .default) { (action:UIAlertAction) in
+            self.openPhotoLibraryButton(sender: self);
+        }
+        
+        let takePhoto = UIAlertAction(title: "Сделать фото", style: .default) { (action:UIAlertAction) in
+            self.openCameraButton(sender: self);
+        }
+        
+        let cancel = UIAlertAction(title: "Cancel", style: .cancel) { (action:UIAlertAction) in
+            print("You've pressed cancel");
+        }
+        
+        alertController.addAction(setFromGallery)
+        alertController.addAction(takePhoto)
+        alertController.addAction(cancel)
+        
+        self.present(alertController, animated: true, completion: nil)
+    }
+    
+    @IBAction func openCameraButton(sender: AnyObject) {
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            print("camera is available")
+            let imagePicker = UIImagePickerController()
+            imagePicker.delegate = self
+            imagePicker.sourceType = .camera;
+            imagePicker.allowsEditing = false
+            self.present(imagePicker, animated: true, completion: nil)
+        }
+        else { print("camera is not available") }
+    }
+    
+    @IBAction func openPhotoLibraryButton(sender: AnyObject) {
+        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
+            let imagePicker = UIImagePickerController()
+            imagePicker.delegate = self
+            imagePicker.sourceType = .photoLibrary;
+            imagePicker.allowsEditing = true
+            self.present(imagePicker, animated: true, completion: nil)
+        }
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        let image = info[UIImagePickerControllerOriginalImage] as! UIImage
+        imagePicked.image = image
+        dismiss(animated:true, completion: nil)
+    }
+
+    
+    @IBAction func saveButton(sender: AnyObject) {
+        print ("save erroooooor")
+        let imageData = UIImageJPEGRepresentation(imagePicked.image!, 0.6)
+        let compressedJPGImage = UIImage(data: imageData!)
+        UIImageWriteToSavedPhotosAlbum(compressedJPGImage!, nil, nil, nil)
+        
+//        let alert = UIAlertView(title: "Wow",
+//                                message: "Your image has been saved to Photo Library!",
+//                                delegate: nil,
+//                                cancelButtonTitle: "Ok")
+//        alert.show()
     }
     
 //    convenience init() {
